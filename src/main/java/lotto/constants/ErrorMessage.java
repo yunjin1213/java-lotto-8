9d0238c0fe503ea9d19ea98113428e818b4d498a
사용자 이름 <dlwhdgur0509@gmail.com>
2026-02-20 11:19:46 +0900

    feat: 구입 금액 검증 및 로또 자동 발행기(LottoMachine) 구현

--- src/main/java/lotto/constants/ErrorMessage.java
+++ src/main/java/lotto/constants/ErrorMessage.java
@@ -3,7 +3,8 @@ package lotto.constants;
 public enum ErrorMessage {
     INVALID_SIZE("로또 번호는 " + LottoRule.LOTTO_SIZE + "개여야 합니다."),
     DUPLICATE_NUMBER("로또 번호는 중복될 수 없습니다."),
-    INVALID_RANGE("로또 번호는 " + LottoRule.MIN_NUMBER + "부터 " + LottoRule.MAX_NUMBER + " 사이여야 합니다.");
+    INVALID_RANGE("로또 번호는 " + LottoRule.MIN_NUMBER + "부터 " + LottoRule.MAX_NUMBER + " 사이여야 합니다."),
+    INVALID_MONEY("로또 구입 금액은 " + LottoRule.TICKET_PRICE + "원 단위여야 합니다.");
 
     private static final String PREFIX = "[ERROR] ";
     private final String message;
